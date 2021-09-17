a9c99ca845706773fb1050cf366e47d590cbaaaa
roy301994 <tristantofir@gmail.com>
2021-09-17 18:30:50 +0700

    add data contact dan error

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const {loadData,findContact}=require('./util/contacts')
+const {loadData,findContact,addContact,checkDuplicateContact}=require('./util/contacts')
+const { body, validationResult ,check} = require('express-validator');
+const session=require('express-session')
+const cookieParser=require('cookie-parser')
+const flash=require('connect-flash')
+
+
+
+//konfigurasi flash
+
+app.use(cookieParser('secret'))
+app.use(
+  session({
+  cookie: { maxAge: 6000 },
+  secret: 'secret cat',
+  resave: false,
+  saveUninitialized: true,
+  })
+)
+
+app.use(flash())
+
+
+
+
+
+
+
+
+
 
 //middleware
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))//data yg dikirim masih undifined harus diparsing
 
 //menggunakan ejs
 app.set('view engine','ejs')
@@ -56,14 +86,68 @@ app.get('/about', (req, res) => {
     const contactsA=loadData()//kita sudah buat methodnya loadData dimana load data akan membaca kontak yg kita buat 
     // console.log(contacts) //diconsole datanya sudah keluar sekarang kita kirim ke view biar keluar diweb kalau di console hanya keluar di terminal saja
     res.render('contact',{
-      contactsB:contactsA  //diubah ke object
+      contactsB:contactsA, //diubah ke object
+      msg:req.flash('msg')  //karena ditampilak di halaman contact maka kita edit juga di ejs contact if msg ada dan else nya
     })
     // res.sendFile('./contact.html',{root: __dirname}) 
   })
 
-  //route untuk detail,yg diatas contact tanpa param yg dibawah contact dengan param
+//proses data contact
+app.post('/contact',[
+  body('nama').custom((value)=>{
+    const duplicate=checkDuplicateContact(value)
+    if (duplicate) {
+      throw new Error('This name is already taken')
+    }
+    return true //fungsi validasi dengan return true artinya valuenya lolos validasi
 
 
+  }),
+  check('email','Your email is not valid').isEmail(),
+  check('noHP','Your phone number is not valid').isMobilePhone('id-ID'),//'noHP' mengikuti dari parameter di noHP pada add-contact ejs dibagian "name" line 24
+],(req,res)=>{
+  const errors = validationResult(req)//dengan validation result dapat mengeluarkan pesan error
+  if (!errors.isEmpty()) {//kalau error tidak kosong maka return errornya
+    // return res.status(400).json({ errors: errors.array() });
+    res.render('add-contact',{  //error akan dikirim ke halaman add-contact di ejs
+      title : 'Form Add Data Contact',
+      errors: errors.array(), //kalau error tidak ada maka errors akan berisi undifined
+    })
+  } else {
+// console.log(req.body)//ditampilkan diterminal
+// res.send(req.body)//ditampilkan di web
+addContact(req.body)
+
+//sebelum redirect kirim flash message dulu
+req.flash('msg','Data has added successfully !')
+//kalau sudah di set message nya lanjut untuk tangkap messagenya dihalaman contact
+
+res.redirect('/contact')//redirect akan membawa ke route app.get dan langsung load seluruh list contact
+  }
+  
+
+
+
+})//app.post untuk menerima data add contact
+
+
+
+
+  
+//halaman form tambah data contact
+
+
+app.get('/contact/add',(req,res)=>{
+  res.render('add-contact')
+})
+
+
+
+
+
+
+//halaman detail contact
+//route untuk detail,yg diatas contact tanpa param yg dibawah contact dengan param
   app.get('/contact/:nama', (req, res) => {
 //mencari kontak yg spesific sesuai dengan nama yg dikirim
     const contactC=findContact(req.params.nama)//kita tidak pake method loadData karena load data menampilkan seluruh data all
--- util/contacts.js
+++ util/contacts.js
@@ -45,8 +45,44 @@ const findContact=(nama)=>{
 }
 
 
-module.exports={loadData,findContact}
+//ada 2 fungsi untuk add Contact,kenapa dipisah karena fungsi 1 akan digunakan kembali untuk hapus dan ubah contact
+//fungsi 1 :menimpa file contact.json dengan data yang baru
+const saveContacts=(contacts)=>{
+fs.writeFileSync('data/contacts.json',JSON.stringify(contacts))//sebelumnya dataJson adalah object  ubah jadi string melalui method JSON.stringify
+//habis itu timpa ke file contact.json
+//kebalikan dari JSON.stringify adalah JSON.parse mengubah dari string ke object
+}
+
+//fungsi2 :menambah contact baru kedalam array,yg arraynya nanti akan ditimpa di fungsi ke 2
+const addContact=(contact)=>{  //gunanya kita buat paramater contact sebagai variable penampung,kalau sudah ditampung bisa diolah
+  const contacts =loadData()
+  contacts.push(contact)
+  //baru panggil disini fungsi 1 biar otomatis nimpa
+  //setelah data contacts tertambah oleh data contact yg baru ,barulah di masukkan ke fungsi saveContact untuk menimpa dan mengganti data contact lama yang ada di file contacts.json
+  saveContacts(contacts)
+
+}
+
+//check nama duplicated contacts
+
+const checkDuplicateContact=(nama)=>{
+  const contacts =loadData()
+  return contacts.find((contact)=>contact.nama===nama)
+}
+
+
+
+
+
+
+
+
+//find itu untuk menelusuri ketika ketemu satu selesai
+
+module.exports={loadData,findContact,addContact,checkDuplicateContact}
 //hanya loadData saja karena datapath dan dirpath tidak akan dikirim untuk digunakan di app.js
 
 //kondisi diawal adalah data/contacts.json tidak ada tapi karena file loadData di panggil di file app.js dan kita nodemon start yang mana menjalankan node appnya
 //maka otomatis langsung create folder data dan file contact.json saat nodemon start dijalankan
+
+//kenapa saveContact tidak dieksport karena hanya dipake dihalaman ini saja di line 62 di fungsi addContact jadi tidak perlu dieksport
